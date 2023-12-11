ceb5fd59a0374da43b70e8fad2a2c007478cc14e
Madhushri Karekar <madhushrikarekar16@gmail.com>
2023-12-11 23:58:50 +0530

    add file

---
+++ day10/app.js
@@ -0,0 +1,4 @@
+const searchForm = document.querySelector("#search-form");
+const searchBox = document.querySelector("#search-box");
+const searchResult = document.querySelector(".search-result");
+const showBtn = document.querySelector(".show-btn");
